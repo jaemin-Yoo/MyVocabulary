b98a7d0f0c408e7ba638419b74de00c2d5cb506d
jaemin-Yoo <iamjm29@naver.com>
2020-09-20 01:28:55 +0900

    MainActivity complete

--- MyVocabulary/app/src/main/java/com/example/myvocabulary/MainActivity.java
+++ MyVocabulary/app/src/main/java/com/example/myvocabulary/MainActivity.java
@@ -2,14 +2,44 @@ package com.example.myvocabulary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button add, test, exit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        add=(Button)findViewById(R.id.btn_add);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, BookActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        test=(Button)findViewById(R.id.btn_test);
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, BookActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        exit=(Button)findViewById(R.id.btn_exit);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
